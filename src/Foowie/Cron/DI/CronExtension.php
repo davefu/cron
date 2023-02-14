31d3bc3a972791180b474fae4a5ba8fb0bef0aa0
David Fiedor <davefu@seznam.cz>
2023-02-14 11:41:56 +0100

    Update to Nette v3.

--- src/Foowie/Cron/DI/CronExtension.php
+++ src/Foowie/Cron/DI/CronExtension.php
@@ -5,58 +5,67 @@ namespace Foowie\Cron\DI;
 use Nette\Configurator;
 use Nette\DI\Compiler;
 use Nette\DI\CompilerExtension;
-use Nette\DI\Statement;
+use Nette\DI\Definitions\Statement;
 use Nette\PhpGenerator\ClassType;
 use Nette\PhpGenerator\Helpers;
+use Nette\Schema\Expect;
+use Nette\Schema\Schema;
 use Nette\Utils\AssertionException;
 use Nette\Utils\Json;
+use stdClass;
 
 /**
  * @author Daniel Robenek <daniel.robenek@me.com>
+ *
+ * @property-read stdClass $config
  */
 class CronExtension extends CompilerExtension {
 
 	const REPOSITORY_NDB = 'ndb';
 	const REPOSITORY_DOCTRINE = 'doctrine';
 
-	public $defaults = array(
-		'cronJobTag' => 'cronJob',
-		'repositoryType' => null, // null = autodetect, false = none
-		'table' => 'cron', // ndb only
+	public function getConfigSchema(): Schema {
+		return Expect::structure([
+			'cronJobTag' => Expect::string('cronJob'),
+			'repositoryType' => Expect::bool()->nullable(), // null = autodetect, false = none
+			'table' => Expect::string('cron'), // ndb only
 
-		'mapPresenter' => true,
-		'maxExecutionTime' => null,
-		'memoryLimit' => null,
-		'securityToken' => false,
-		'mapping' => array(
-			'FoowieCron' => 'Foowie\\Cron\\Application\\UI\\*\\*Presenter'
-		),
-		'router' => array(
-			'pattern' => 'cron[/<token>]',
-			'metadata' => 'FoowieCron:Cron:default',
-		),
-		'jobs' => array(),
-		'panel' => true,
-	);
+			'mapPresenter' => Expect::bool(true),
+			'maxExecutionTime' => Expect::int()->nullable(),
+			'memoryLimit' => Expect::string()->nullable(),
+			'securityToken' => Expect::anyOf(Expect::string()->nullable(), Expect::bool(false)),
+			'mapping' => Expect::array([
+					'FoowieCron' => 'Foowie\\Cron\\Application\\UI\\*\\*Presenter'
+				]
+			),
+			'router' => Expect::array([
+					'pattern' => 'cron[/<token>]',
+					'metadata' => 'FoowieCron:Cron:default',
+				]
+			),
+			'jobs' => Expect::array(),
+			'panel' => Expect::bool(true),
+		]);
+	}
 
 	public function loadConfiguration() {
 
 		$builder = $this->getContainerBuilder();
 
-		$config = $this->getConfig($this->defaults);
+		$config = $this->config;
 
 		$factory = $builder->addDefinition($this->prefix('contextCronFactory'))
-			->setClass('Foowie\Cron\ContextCronFactory')
-			->setArguments(array('@\Nette\DI\Container', $config['cronJobTag']));
+			->setType('Foowie\Cron\ContextCronFactory')
+			->setArguments(['@\Nette\DI\Container', $config->cronJobTag]);
 
 		$builder->addDefinition($this->prefix('cron'))
-			->setClass('Foowie\Cron\ICron')
-			->setFactory(array($factory, 'create'));
+			->setType('Foowie\Cron\ICron')
+			->setFactory([$factory, 'create']);
 
 		$builder->addDefinition($this->prefix('dateTimeProvider'))
-			->setClass('Foowie\Cron\DateTime\SystemDateTimeProvider');
+			->setType('Foowie\Cron\DateTime\SystemDateTimeProvider');
 
-		if ($config['mapPresenter']) {
+		if ($config->mapPresenter) {
 			$this->loadMapping();
 		}
 
@@ -68,8 +77,8 @@ class CronExtension extends CompilerExtension {
 
 	public function afterCompile(ClassType $class) {
 		$container = $this->getContainerBuilder();
-		$config = $this->getConfig($this->defaults);
-		if ($config['panel'] && $container->parameters['debugMode']) {
+		$config = $this->config;
+		if ($config->panel && $container->parameters['debugMode']) {
 			$init = $class->methods['initialize'];
 			$init->addBody(Helpers::format(
 				'Foowie\Cron\Diagnostics\Panel::register($this->getByType(?), $this->getByType(?));',
@@ -81,40 +90,39 @@ class CronExtension extends CompilerExtension {
 
 	protected function loadMapping() {
 		$builder = $this->getContainerBuilder();
-		$config = $this->getConfig($this->defaults);
+		$config = $this->config;
 
 		if ($config['securityToken'] === false) {
 			throw new AssertionException('Specify security token [' . $this->name . '.securityToken] please.');
 		}
 		$builder->addDefinition($this->prefix('securityToken'))
-			->setClass('Foowie\Cron\Application\UI\SecurityToken', array($config['securityToken']));
+			->setFactory('Foowie\Cron\Application\UI\SecurityToken', [$config->securityToken]);
 		$builder->addDefinition($this->prefix('router'))
-			->setClass('Foowie\Cron\Application\Routers\Route', array($config['router']['pattern'], $config['router']['metadata']))
+			->setFactory('Foowie\Cron\Application\Routers\Route', [$config->router->pattern, $config->router->metadata])
 			->setAutowired(false);
 		$builder->getDefinition('router')
-			->addSetup('Foowie\Cron\Application\Routers\Route::prependToRouteList($service, ?)', array($this->prefix('@router')));
+			->addSetup('Foowie\Cron\Application\Routers\Route::prependToRouteList($service, ?)', [$this->prefix('@router')]);
 		$builder->getDefinition('nette.presenterFactory')
-			->addSetup('setMapping', array($config['mapping']));
+			->addSetup('setMapping', [$config->mapping]);
 		$builder->addDefinition($this->prefix('cronPresenter'))
-			->setClass('Foowie\Cron\Application\UI\CronPresenter')
-			->addSetup('setMaxExecutionTime', [$config['maxExecutionTime']])
-			->addSetup('setMemoryLimit', [$config['memoryLimit']]);
+			->setType('Foowie\Cron\Application\UI\CronPresenter')
+			->addSetup('setMaxExecutionTime', [$config->maxExecutionTime])
+			->addSetup('setMemoryLimit', [$config->memoryLimit]);
 	}
 
 	protected function loadRepository() {
 		$builder = $this->getContainerBuilder();
-		$config = $this->getConfig($this->defaults);
+		$config = $this->config;
 
-		$repositoryType = $config['repositoryType'] === null ? $this->detectRepositoryType() : $config['repositoryType'];
+		$repositoryType = $config->repositoryType === null ? $this->detectRepositoryType() : $config->repositoryType;
 		switch ($repositoryType) {
 			case static::REPOSITORY_DOCTRINE:
 				$builder->addDefinition($this->prefix('repository'))
-					->setClass('Foowie\Cron\Repository\Doctrine2Repository');
+					->setType('Foowie\Cron\Repository\Doctrine2Repository');
 				break;
 			case static::REPOSITORY_NDB:
 				$builder->addDefinition($this->prefix('repository'))
-					->setClass('Foowie\Cron\Repository\NetteDatabaseRepository')
-					->setArguments(array("@\Nette\Database\Context", $config['table']));
+					->setFactory('Foowie\Cron\Repository\NetteDatabaseRepository', ["@\Nette\Database\Context", $config->table]);
 				break;
 			case false:
 				break;
@@ -125,13 +133,13 @@ class CronExtension extends CompilerExtension {
 
 	protected function loadJobs() {
 		$builder = $this->getContainerBuilder();
-		$config = $this->getConfig($this->defaults);
+		$config = $this->config;
 
-		foreach ($config['jobs'] as $subscriber) {
+		foreach ($config->jobs as $subscriber) {
 			$def = $builder->addDefinition($this->prefix('job.' . md5(Json::encode($subscriber))));
-			list($def->factory) = Compiler::filterArguments(array(
+			list($def->factory) = Compiler::filterArguments([
 				is_string($subscriber) ? new Statement($subscriber) : $subscriber
-			));
+			]);
 
 			list($subscriberClass) = (array)$builder->normalizeEntity($def->factory->entity);
 			if (class_exists($subscriberClass)) {
@@ -139,7 +147,7 @@ class CronExtension extends CompilerExtension {
 			}
 
 			$def->setAutowired(false);
-			$def->addTag($config['cronJobTag']);
+			$def->addTag($config->cronJobTag);
 		}
 	}
 
